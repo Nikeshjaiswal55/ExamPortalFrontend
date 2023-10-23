c2f8d31c6cc18f0ebc4b3527678c554afee77a24
Piyush Dixit <piyushd.bca2022@ssism.org>
2023-10-23 15:19:00 +0530

    added a welcome to Online exam portal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import { Routes } from './routes/Routes';
+import {Routes} from './routes/Routes';
 
 const App = () => {
-  return <Routes />;
+  return (
+
+    <div>
+      <Routes />
+      <h1>welcome to Online Exam Portal</h1>
+    </div>
+  );
 };
 
 export default App;
