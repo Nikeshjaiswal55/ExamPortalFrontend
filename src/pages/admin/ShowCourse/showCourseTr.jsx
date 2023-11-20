a18133e28864d7d8578f9cd79b04951e144928ec
Piyush Dixit <piyushd.bca2022@ssism.org>
2023-11-20 14:58:09 +0530

    added api  intergration on show courses

--- src/pages/admin/ShowCourse/showCourseTr.jsx
+++ src/pages/admin/ShowCourse/showCourseTr.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const styleRow = {width: "150px",color: "#4E4E4E",fontSize: "16px"};
+const styleRow = {minWidth: "150px",color: "#4E4E4E",fontSize: "16px"};
 export function ShowCourseTr(props) {
     return (
         <>
-            <tr className='d-flex justify-content-md-around justify-content-start text-center  gx-3 align-items-baseline py-1'>
-                <td className='text-capitalize flex-shrink-0 border-0  text-nowrap  ' style={styleRow}>{props.assesmentName}</td>
-                <td className='text-capitalize flex-shrink-0 border-0 ' style={styleRow}>{props.createdBy}</td>
+            <tr className='d-flex justify-content-md-around justify-content-start   gx-3 align-items-baseline py-1'>
+                <td className='custom-table-td-width text-capitalize flex-shrink-0 border-0  text-nowrap  '>{props.assesmentName}</td>
+                <td className='custom-table-td-width text-capitalize flex-shrink-0 border-0 '>{props.createdBy}</td>
             </tr>
         </>
     )
