a0979c1f45a25683aa9aa8b55aca8fe3037850ad
akasht.bba2022@ssism.org <akasht.bba2022@ssism.org>
2023-12-05 11:18:07 +0530

    admindashbordgraph

---
+++ src/pages/home/AdminDashBoardGraph.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import AdminDashboard from "./admindashboard/AdminDashboard";
+
+
+function AdminDashboardGraph() {
+  
+
+    return (
+      <>
+      <div className=" ">
+     <AdminDashboard />
+     </div>
+      </>
+    )
+  }
+  
+  export default AdminDashboardGraph;
+  
