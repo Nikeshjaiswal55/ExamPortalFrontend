e1c97495ed5168b2394a17149af907429a82dc9d
Piyush Dixit <piyushd.bca2022@ssism.org>
2023-12-07 17:18:38 +0530

    feat : add download certificate, fix ui dashboard

--- src/pages/home/AdminDashBoard/AdminDashboards.jsx
+++ src/pages/home/AdminDashBoard/AdminDashboards.jsx
@@ -4,7 +4,7 @@ import '../component/Chart.css'
 import {FaUsers,FaShoppingCart} from "react-icons/fa";
 import {MdOutlineAssignment} from "react-icons/md";
 import {CustomButton} from '../../../theme/Button/Buttons'
-import {useGetAssignmentQuery,useGetTop5AssissmentQuery} from '../../../apis/Service';
+import {useGetAssignmentQuery,useGetTop3AssissmentStudentsQuery,useGetTop5AssissmentQuery} from '../../../apis/Service';
 import {SubIdSplit} from '../../../utils/SubIdSplit';
 import BarChart from '../component/BarChart';
 import DoughnutChart from '../component/DoughnutChart';
@@ -47,19 +47,19 @@ const info = [
 ];
 const TopStudent = [
   {
-    id: 12,
+    studentid: 12,
     email: "dixitp034@gmail.com",
   },
   {
-    id: 13,
+    studentid: 13,
     email: "kapilj.bca2022@ssism.org",
   },
   {
-    id: 14,
+    studentid: 14,
     email: "akashbba2022@ssism.org",
   }
 ]
-function TotalComponent({infoText,infoNumber,icon,iconClassName}) {
+const TotalComponent = ({infoText,infoNumber,icon,iconClassName}) => {
   return <>
     <div class="col-12 col-sm-6 h-7 col-md-4 p-2 m-0" style={{height: "150px"}}>
       <div class=" w-100  d-flex align-items-center gap-2   border m-1 p-2 rounded-2" style={{backgroundColor: "var(--main-div)"}}>
@@ -73,20 +73,19 @@ function TotalComponent({infoText,infoNumber,icon,iconClassName}) {
   </>
 }
 
-function TopStudentCard({email,onEvidenceClick}) {
+const TopStudentCard = ({email,onEvidenceClick}) => {
   const randomColor = color[Math.floor(Math.random() * color.length)];
   return <>
-    <div class="col-12  p-2 m-0">
-      <div class=" w-100 h-100 d-flex align-items-center gap-2  border m-1 p-2 rounded-2">
+    <div class="col-12  p-1 m-0">
+      <div class=" w-100 h-100 d-flex align-items-center gap-2  border m-1 p-1 rounded-2">
         <span class={` h-100 w-25 d-flex justify-content-center align-items-center rounded-2   elevation-1 `}>
-
           <div
             className={` ${randomColor} img-thumbnail d-flex justify-content-center align-items-center rounded-circle  m-auto p-0   border-dark border-1 `}
-            style={{width: "70px",height: "70px"}}
+            style={{width: "40px",height: "40px"}}
           >
-            <h2 className="text-capitalize text-light">
+            <h4 className="text-capitalize text-light p-0 ">
               {email.charAt(0).toUpperCase()}
-            </h2>
+            </h4>
           </div> </span>
         <div class=" w-100  d-flex flex-wrap   justify-content-between align-items-center align-items-center">
           <span class="info-box-text me-2 fs-6">{email}</span>
@@ -97,10 +96,33 @@ function TopStudentCard({email,onEvidenceClick}) {
   </>
 }
 
+const TopStudentAcordianItem = ({index,assessmentId,assessmentName,examDuration,examDate}) => {
+  // const {data: TopStudent,isLoading,isError,isSuccess} = useGetTop3AssissmentStudentsQuery(assessmentId);
+  return (
+    <>
+      <Accordion.Item eventKey={index} className=' border-0  border-bottom b'>
+        <Accordion.Header className=' border border-white'>   <div className='row w-100  gap-2 gap-lg-0 fw-bold '>
+          <div className='  col-md-4   '>{assessmentName} </div>
+          <div className='  col-md-4  '>{examDuration} </div>
+          <div className='  col-md-4   align-self-end'>{examDate} </div>
+        </div></Accordion.Header>
+        <Accordion.Body className='px-2 py-0 w-100'>
+          <div className='row w-100'>
+            {TopStudent && TopStudent.map((value) => {
+              return <TopStudentCard key={value.studentid} email={value.email} onEvidenceClick={() => {console.log("evidence click")}} />
+            })
+            }
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+    </>
+  )
+}
 
 export const AdminDashboard = () => {
   let userId = JSON.parse(localStorage.getItem('users'));
   userId = SubIdSplit(userId.sub);
+
   const {
     data,
     isLoading,
@@ -109,7 +131,7 @@ export const AdminDashboard = () => {
   } = useGetAssignmentQuery({id: userId});
   const {data: top5Assessment
     ,isLoading: isTop5AssessmentLoading
-    ,isError: isTop5AssessmentError} = useGetTop5AssissmentQuery(5);
+    ,isError: isTop5AssessmentError} = useGetTop5AssissmentQuery(userId);
 
   // console.log("data :-  ",data);
   // console.log("userId :-  ",userId);
@@ -179,25 +201,37 @@ export const AdminDashboard = () => {
                 <div className=' w-100 py-3 p-md-2   fw-bold '>
                   <h4 className=' ps-3'> Active Assessment</h4>
                 </div>
-                <Accordion className='  my-2 p-0 p-md-2 ' >
+                  <Accordion className='  my-1 p-0  ' >
 
                   {data && data?.map((value,index) => {
-                    return <Accordion.Item eventKey={index} className=' border-0  border-bottom b'>
-                      <Accordion.Header className=' border border-white'>   <div className=' w-100 d-flex  gap-2  flex-wrap justify-content-around fw-bold '>
-                        <span className=' '>{value?.assessmentName} </span>
-                        <span>{value?.examDuration} </span>
-                        <span className=' align-self-end'>28-sep-2023 </span>
-                      </div></Accordion.Header>
-                      <Accordion.Body>
-                        <div className='row'>
-                          {TopStudent && TopStudent.map((value) => {
-                            return <TopStudentCard key={value.id} email={value.email} onEvidenceClick={() => {console.log("evidence click")}} />
-                          })
-                          }
-                        </div>
-                      </Accordion.Body>
-                    </Accordion.Item>
-                  })
+                    // return <Accordion.Item eventKey={index} className=' border-0  border-bottom b'>
+                    //   <Accordion.Header className=' border border-white'>   <div className='row w-100  gap-2 gap-lg-0 fw-bold '>
+                    //     <div className='  col-md-4   '>{value?.assessmentName} </div>
+                    //     <div className='  col-md-4  '>{value?.examDuration} </div>
+                    //     <div className='  col-md-4   align-self-end'>28-sep-2023 </div>
+                    //   </div></Accordion.Header>
+                    //   <Accordion.Body>
+                    //     <div className='row'>
+                    //       {TopStudent && TopStudent.map((value) => {
+                    //         return <TopStudentCard key={value.id} email={value.email} onEvidenceClick={() => {console.log("evidence click")}} />
+                    //       })
+                    //       }
+                    //     </div>
+                    //   </Accordion.Body>
+                    // </Accordion.Item>
+                    if(value._Active) {
+                      return <TopStudentAcordianItem
+                        index={index}
+                        assessmentId={value?.paperId}
+                        assessmentName={value?.assessmentName}
+                        examDate={"28-09-2033"}
+                        examDuration={value?.examDuration} />
+                    } else {
+                      return null;
+                    }
+                  }
+
+                    )
                   }
 
                 </Accordion>
--- src/pages/home/StudentDashBoard/StudentDashBoard.jsx
+++ src/pages/home/StudentDashBoard/StudentDashBoard.jsx
@@ -1,68 +1,49 @@
 import React,{useEffect,useState} from 'react';
 import {Accordion} from 'react-bootstrap';
 import '../component/Chart.css'
-import {FaUsers,FaShoppingCart} from "react-icons/fa";
+import {FaUsers,FaThumbsUp} from "react-icons/fa";
+
 import {MdOutlineAssignment} from "react-icons/md";
 import {CustomButton} from '../../../theme/Button/Buttons'
-import {useGetAssignmentQuery,useGetTop5AssissmentQuery} from '../../../apis/Service';
+import {useGetAllAssissmentOnstudentPageQuery,useGetAssignmentQuery,useGetTop5AssissmentQuery} from '../../../apis/Service';
 import {SubIdSplit} from '../../../utils/SubIdSplit';
 import BarChart from '../component/BarChart';
 import DoughnutChart from '../component/DoughnutChart';
 // import SomethingWentWrong from '../../../components/SomethingWentWrong/SomethingWentWrong';
 import PieChart from '../component/PieChart';
 import {Loader} from '../../../components/Loader/Loader';
+import {StudentCertificate} from '../../student/StudentCertificate/StudentCertificateDownload';
 
-const color = [
-    'bg-danger',
-    'bg-secondary',
-    'bg-primary',
-    'bg-warning',
-    'bg-info',
-    'bg-dark',
-];
-// const randomColor = color[Math.floor(Math.random() * color.length)];
+
+
 const info = [
     {
         id: 1,
-        infoText: "Total Student",
+        infoText: "given Assessement",
         infoNumber: "10,333",
         icon: <FaUsers size={50} />,
         iconClassName: "bg-warning",
     },
     {
         id: 2,
-        infoText: "Total Assessment",
+        infoText: " Pending Assessment",
         infoNumber: "10,333",
         icon: <MdOutlineAssignment size={50} />,
         iconClassName: "bg-danger",
     },{
         id: 3,
-        infoText: "like",
+        infoText: "Passed Assessment",
         infoNumber: "10,333",
-        icon: <FaShoppingCart size={50} />,
+        icon: <FaThumbsUp size={50} />,
         iconClassName: "bg-success",
 
     }
 ];
-// const TopStudent = [
-//     {
-//         id: 12,
-//         email: "dixitp034@gmail.com",
-//     },
-//     {
-//         id: 13,
-//         email: "kapilj.bca2022@ssism.org",
-//     },
-//     {
-//         id: 14,
-//         email: "akashbba2022@ssism.org",
-//     }
-// ]
 
 function TotalComponent({infoText,infoNumber,icon,iconClassName}) {
     return <>
         <div class="col-12 col-sm-6 h-7 col-md-4 p-2 m-0" style={{height: "150px"}}>
-            <div class=" w-100  d-flex align-items-center gap-2  bg-white  border m-1 p-2 rounded-2">
+            <div class=" w-100  d-flex align-items-center gap-2   border m-1 p-2 rounded-2" style={{backgroundColor: "var(--main-div)"}} >
                 <span class={` d-flex justify-content-center align-items-center rounded-2   elevation-1 ${iconClassName}  `} style={{width: "80px ",height: "80px"}}>{icon}</span>
                 <div class="  d-flex flex-column">
                     <span class="info-box-text fs-4 fw-bold">{infoText}</span>
@@ -73,33 +54,14 @@ function TotalComponent({infoText,infoNumber,icon,iconClassName}) {
     </>
 }
 
-function TopStudentCard({email,onEvidenceClick}) {
-    const randomColor = color[Math.floor(Math.random() * color.length)];
-    return <>
-        <div class="col-12  p-2 m-0">
-            <div class=" w-100 h-100 d-flex align-items-center gap-2  border m-1 p-2 rounded-2">
-                <span class={` h-100 w-25 d-flex justify-content-center align-items-center rounded-2   elevation-1 `}>
-
-                    <div
-                        className={` ${randomColor} img-thumbnail d-flex justify-content-center align-items-center rounded-circle  m-auto p-0   border-dark border-1 `}
-                        style={{width: "70px",height: "70px"}}
-                    >
-                        <h2 className="text-capitalize text-light">
-                            {email.charAt(0).toUpperCase()}
-                        </h2>
-                    </div> </span>
-                <div class=" w-100  d-flex flex-wrap   justify-content-between align-items-center align-items-center">
-                    <span class="info-box-text me-2 fs-6">{email}</span>
-                    <CustomButton buttonText={"Evidence"} onButtonClick={onEvidenceClick} />
-                </div>
-            </div>
-        </div>
-    </>
-}
 
 export const StudentDashBoard = () => {
     let userId = JSON.parse(localStorage.getItem('users'));
     userId = SubIdSplit(userId.sub);
+    const {data} = useGetAllAssissmentOnstudentPageQuery(userId);
+    useEffect(() => {
+        console.log("data of student page all assessment ",data);
+    },[data]);
     const {
         data: xy,
         isLoading,
@@ -115,24 +77,24 @@ export const StudentDashBoard = () => {
 
     const [barChatData,setBarChatData] = useState();
     const [barChartLabels,setBarChatLabels] = useState();
-    const data = [
-        {
-            id: 1,
-            assessmentName: " given assessment",
-            assessmentDate: " 22-08-2022",
-            assessmentDuration: " 22:08:20",
-        },{
-            id: 2,
-            assessmentName: " given assessment2",
-            assessmentDate: " 22-08-2022",
-            assessmentDuration: " 22:08:20",
-        },{
-            id: 3,
-            assessmentName: " given assessment3",
-            assessmentDate: " 22-08-2022",
-            assessmentDuration: " 22:08:20",
-        }
-    ]
+    // const data = [
+    //     {
+    //         id: 1,
+    //         assessmentName: " given assessment",
+    //         assessmentDate: " 22-08-2022",
+    //         assessmentDuration: " 22:08:20",
+    //     },{
+    //         id: 2,
+    //         assessmentName: " given assessment2",
+    //         assessmentDate: " 22-08-2022",
+    //         assessmentDuration: " 22:08:20",
+    //     },{
+    //         id: 3,
+    //         assessmentName: " given assessment3",
+    //         assessmentDate: " 22-08-2022",
+    //         assessmentDuration: " 22:08:20",
+    //     }
+    // ]
     useEffect(() => {
         const chartData = [];
         const chartLabels = [];
@@ -163,19 +125,19 @@ export const StudentDashBoard = () => {
                         <div className=" w-100 h-100 m-0  p-0 g-md-0 overflow-auto bg-transparent">
                             <div className="row w-100  chart-box   p-0 m-0 " style={{height: "20rem"}}>
                                 <div className="col-12 col-md-4  d-flex  flex-column flex-lg-row    align-items-center   justify-content-center rounded-3">
-                                    {barChatData && barChartLabels && <div className=' chart-parent w-100 h-75  d-flex align-items-center  bg-white rounded-3 py-3 justify-content-center '>
+                                        {barChatData && barChartLabels && <div className=' chart-parent w-100 h-75  d-flex align-items-center   rounded-3 py-3 justify-content-center ' style={{backgroundColor: "var(--main-div)"}}>
                                         <BarChart chartLabels={barChartLabels} chartData={barChatData} />
                                     </div>}
                                 </div>
                                 <div className="col-12 col-md-4 h-auto  d-flex   align-items-center    justify-content-center    rounded-3">
 
-                                    {barChatData && barChartLabels && <div className=' chart-parent w-100 h-75  d-flex   align-items-center bg-white rounded-3 py-3  justify-content-center '>
+                                        {barChatData && barChartLabels && <div className=' chart-parent w-100 h-75  d-flex   align-items-center  rounded-3 py-3  justify-content-center ' style={{backgroundColor: "var(--main-div)"}}>
                                         <DoughnutChart DoughnutChartData={barChatData} DoughnutChartLabels={barChartLabels} />
                                     </div>}
 
                                 </div>
                                 <div className="col-12 col-md-4 d-flex   align-items-center   justify-content-center    rounded-3">
-                                    {barChatData && barChartLabels && <div className=' chart-parent w-100 h-75   d-flex   align-items-center  bg-white rounded-3 py-3 justify-content-center '>
+                                        {barChatData && barChartLabels && <div className=' chart-parent w-100 h-75   d-flex   align-items-center   rounded-3 py-3 justify-content-center ' style={{backgroundColor: "var(--main-div)"}}>
                                         <PieChart PieChartData={barChatData} PieChartLabels={barChartLabels} />
                                     </div>}
                                 </div>
@@ -198,13 +160,13 @@ export const StudentDashBoard = () => {
 
                                     {data && data?.map((value,index) => {
                                         return <>
-                                            <div className=' border rounded py-3 px-2 my-1'>   <div className=' w-100 d-flex  flex-column flex-md-row gap-2  flex-wrap  justify-content-start  align-items-start  align-items-md-center justify-content-md-between fw-bold '>
-                                                <div className=' ps-3 ps-md-0 w-auto text-start text-capitalize'>{value?.assessmentName} </div>
-                                                <div className=' ps-3 ps-md-0 w-auto  text-start text-capitalize'>{value?.assessmentDuration} </div>
-                                                <div className=' ps-3 ps-md-0 w-auto  text-start text-capitalize' >{value?.assessmentDate} </div>
-                                                <div className='    d-flex  justify-content-around justify-content-lg-between gap-2'>
+                                            <div className=' border rounded py-3 px-2 my-1'>   <div className=' row gap-2 gap-md-0 w-100  fw-bold '>
+                                                <div className=' col-md-6 col-lg-3  text-capitalize'>{value?.assessmentName} </div>
+                                                <div className=' col-md-6 col-lg-3  text-capitalize'>{value?.examDuration} </div>
+                                                <div className=' col-md-6 col-lg-3  text-capitalize' >{value?.assessmentDate} </div>
+                                                <div className=' col-md-6 col-lg-3  d-flex  justify-content-around  gap-2'>
                                                     <CustomButton buttonText={"Check Result"} />
-                                                    <CustomButton buttonText={"Download Result"} />
+                                                    <StudentCertificate />
                                                 </div>
                                             </div></div>
                                         </>
