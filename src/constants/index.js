04f50c0010eb43f2e7f727da3d337eb0d77a3732
Nikesh Jaiswal <nikeshj.bca2021@ssism.org>
2023-12-21 12:49:28 +0530

    Update AUTH0 REDIRECT_URL

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,5 +3,5 @@ export const AUTH0KEYS = {
     CLIENT_ID: "lA9qLAs5xQxoBuCVM1AJERQoPIsopevs",
     AUDIENCE: "https://exam-easy",
     // REDIRECT_URL: "http://localhost:5173/get-started"
-    REDIRECT_URL: "https://exam-easy.netlify.app/get-started"
+    REDIRECT_URL: "https://exam-portal-frontend-cy59veijx-nikeshjbca2021-ssismorg.vercel.app/get-started"
 }
