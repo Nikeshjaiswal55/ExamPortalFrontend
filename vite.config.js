f578a1ddf45671503f8457484fc22835726470a5
Kuldeep panwar <kuldeeppa war7697@gmail.com>
2024-02-05 23:49:11 +0530

    Add homepage URL and update base path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
 
   plugins: [react()],
+  base:"/exams/",
   build: {
     rollupOptions: {
       external: ['@tensorflow/tfjs-data','@tensorflow/tfjs-layers','@tensorflow-models/speech-commands',' @tensorflow-models/facemesh@0.0.5']
