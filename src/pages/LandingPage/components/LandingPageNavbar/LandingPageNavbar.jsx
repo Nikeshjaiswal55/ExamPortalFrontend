75970fbdc8fcbe5c47d45657326b88e38727b19c
AyushiAgrawal1 <ayushiagrawal469@gmail.com>
2024-02-09 10:34:00 +0530

    add design for responsive login button

--- src/pages/LandingPage/components/LandingPageNavbar/LandingPageNavbar.jsx
+++ src/pages/LandingPage/components/LandingPageNavbar/LandingPageNavbar.jsx
@@ -1,22 +1,29 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { motion } from 'framer-motion';
-import React from 'react';
+import { BiLogIn } from "react-icons/bi";
 import exameasy_light_logo from '../../../../assets/exameasy_light_logo.svg'
 const LandingPageNavbar = () => {
   const { loginWithRedirect } = useAuth0();
+  const imageSize = {
+    width:"12rem"
+  }
 
   return (
-    <div className="navbar container px-3 px-md-0">
+    <div className="navbar row m-auto mt-2 container px-3 px-md-0">
+      <div className="col-8">
       <motion.span animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} initial={{ opacity: 0, scale: 0.5 }}>
-        <h3 className="text-capitalize fw-bold"><img className='img-fluid' src={exameasy_light_logo} alt="logo" width={"50%"} /></h3>
+        <h3 className="text-capitalize fw-bold"><img className='img-fluid' src={exameasy_light_logo} alt="logo" style={imageSize} /></h3>
       </motion.span>
-      <motion.div className="wrapper ">
-        <button className="login-button px-4 px-md-5 py-1" onClick={() => loginWithRedirect()}>
-          Login
-        </button>
+      </div>
+      <div className="col-4">
+      <motion.div className="wrapper d-flex align-item-center">
+        <button className="login-button px-4 px-md-5 py-1 d-none d-md-block" onClick={() => loginWithRedirect()}>Login </button>
+        <BiLogIn className="d-md-none d-lg-none d-xl-none d-sm-block h1 m-auto"  onClick={() => loginWithRedirect()} />
       </motion.div>
+      </div>
     </div>
   );
 };
 
 export default LandingPageNavbar;
+1
