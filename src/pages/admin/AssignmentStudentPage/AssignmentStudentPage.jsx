68f29a7717fbef4e6d9f7ed895c60850d3357687
Himanshivishwakarma <himanshiv.bca2021@ssism.org>
2025-01-30 10:56:35 +0530

    add feat search in student assisment

--- src/pages/admin/AssignmentStudentPage/AssignmentStudentPage.jsx
+++ src/pages/admin/AssignmentStudentPage/AssignmentStudentPage.jsx
@@ -16,6 +16,11 @@ export default function AssignmentStudentPage() {
     console.log('student', data);
   }
 
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredStudents = data?.filter((student) =>
+    student?.name?.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="h-100 w-100 m-0 p-0 ">
       {isError && <SomethingWentWrong />}
@@ -25,6 +30,7 @@ export default function AssignmentStudentPage() {
         </div>
       ) : (
         <>
+        <div className='d-flex justify-content-between align-items-center'>
           <div className="w-100 d-flex gap-2 align-items-center rounded-3 p-2">
             <IoIosArrowBack
               size={30}
@@ -35,9 +41,18 @@ export default function AssignmentStudentPage() {
               Student
             </h4>
           </div>
+          <div style={{ width: "12rem" }}>
+              <input
+                placeholder="Search student.."
+                className="form-control w-100"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+            </div>
+          </div>
             <div className="m-0 p-0 overflow-x-hidden overflow-y-auto" style={{height: 'calc(100vh - 122px)'}}>
             <div className="card-div row w-100 justify-content-center justify-content-md-start mx-2 overflow-auto">
-              {data?.map((studentdetails, index) => (
+              {filteredStudents?.map((studentdetails, index) => (
                 <StudentCard
                   key={index}
                   paperId={paperId}
