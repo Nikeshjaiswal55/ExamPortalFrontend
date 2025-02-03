e4fe070144e7fdc2c1afa398544fb95ab8191144
Himanshivishwakarma <himanshiv.bca2021@ssism.org>
2025-02-03 07:23:28 +0530

    add demo baseUrl.

--- src/apis/Service.js
+++ src/apis/Service.js
@@ -5,7 +5,7 @@ import { LocalStorageCache } from '@auth0/auth0-spa-js';
 // Define a service using a base URL and expected endpoints
 // const baseUrl = " http://localhost:9091"
 // const baseUrl = "https://exameasy-krishna.onrender.com"
-const baseUrl = "https://examsapi.ssism.org/";
+const baseUrl = "https://examsdemoapi.ssism.org/";
 // const baseUrl = "http://192.168.31.195:9091"
 // const baseUrl = "http://192.168.155.155:9091"
 
